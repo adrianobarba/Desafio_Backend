71101b4d44a5210a8a008f015dc1dc93fe727b60
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-09-15 10:28:15 -0300

    criando a fila no rabbitmq

---
+++ src/main/java/barbosa/adriano/btgpactual/orderms/dto/OrderItemEvent.java
@@ -0,0 +1,8 @@
+package barbosa.adriano.btgpactual.orderms.dto;
+
+import java.math.BigDecimal;
+
+public record OrderItemEvent(String produto,
+                             Integer quantidade,
+                             BigDecimal preco) {
+}
