3d68023f93a85e8f497389fdd10a2f115526a1b9
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-09-15 19:33:29 -0300

    commit final

--- src/main/java/barbosa/adriano/btgpactual/orderms/repository/OrderRepository.java
+++ src/main/java/barbosa/adriano/btgpactual/orderms/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package barbosa.adriano.btgpactual.orderms.repository;
 
+import barbosa.adriano.btgpactual.orderms.controller.dto.OrderResponse;
 import barbosa.adriano.btgpactual.orderms.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OrderRepository extends MongoRepository<OrderEntity, Long> {
+    Page<OrderEntity> findByCustomerId(Long customerId, PageRequest pageRequest);
 }
--- src/main/java/barbosa/adriano/btgpactual/orderms/service/OrderService.java
+++ src/main/java/barbosa/adriano/btgpactual/orderms/service/OrderService.java
@@ -1,21 +1,32 @@
 package barbosa.adriano.btgpactual.orderms.service;
 
+import barbosa.adriano.btgpactual.orderms.controller.dto.OrderResponse;
 import barbosa.adriano.btgpactual.orderms.dto.OrderCreatedEvent;
 import barbosa.adriano.btgpactual.orderms.entity.OrderEntity;
 import barbosa.adriano.btgpactual.orderms.entity.OrderItem;
 import barbosa.adriano.btgpactual.orderms.repository.OrderRepository;
+import org.bson.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -27,6 +38,24 @@ public class OrderService {
         entity.setTotal(getTotal(event));
 
         orderRepository.save(entity);
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
+        var orders = orderRepository.findByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations,"tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
